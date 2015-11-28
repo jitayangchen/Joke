7ca6b4b9e07b3e7ccb25de8ce70b2703af9990af
Yangchen <jitayangchen@163.com>
2015-11-28 21:54:01 +0800

    lalala

--- Joke/app/src/main/java/com/pepoc/joke/net/http/HttpRequestManager.java
+++ Joke/app/src/main/java/com/pepoc/joke/net/http/HttpRequestManager.java
@@ -77,7 +77,7 @@ public class HttpRequestManager {
 
 			@Override
 			public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-
+				request.getOnHttpResponseListener().onError();
 			}
 		});
 	}
@@ -86,16 +86,24 @@ public class HttpRequestManager {
 	 * 发送Post请求
 	 * @param request
 	 */
-	private void sendPostRequest(HttpRequest request) {
+	private void sendPostRequest(final HttpRequest request) {
 		asyncHttpClient.post(request.context, request.getURL(), requestParamsConvert(request.getParams()), new AsyncHttpResponseHandler() {
 			@Override
 			public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
 				String response = new String(responseBody);
+				Logger.i("Http response result <<<<<<<<<<<<<<< " + response);
+				Object result = null;
+				try {
+					result = request.parseResponseResult(response);
+				} catch (JSONException e) {
+					e.printStackTrace();
+				}
+				request.getOnHttpResponseListener().onHttpResponse(result);
 			}
 
 			@Override
 			public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-
+				request.getOnHttpResponseListener().onError();
 			}
 		});
 	}
@@ -115,103 +123,6 @@ public class HttpRequestManager {
 		return requestParams;
 	}
 
-//	/**
-//	 * 发送Http请求
-//	 * @param request
-//	 */
-//	public void sendRequest(final HttpRequest request) {
-//		String requestUrl = createGetRequestUrl(request);
-//		Logger.i("Http request url >>>>>>>>>>>>>>> " + requestUrl);
-//
-//		if (HttpRequest.METHOD_GET.equals(request.getRequestMethod())) {
-//			sendGetRequest(requestUrl, new Listener<String>() {
-//
-//				@Override
-//				public void onResponse(String response) {
-//					Logger.i("Http response result <<<<<<<<<<<<<<< " + response);
-//					Object result = request.parseResponseResult(response);
-//					request.getOnHttpResponseListener().onHttpResponse(result);
-//				}
-//
-//			}, new ErrorListener() {
-//
-//				@Override
-//				public void onErrorResponse(VolleyError error) {
-//					request.getOnHttpResponseListener().onError();
-//				}
-//			});
-//		} else if (HttpRequest.METHOD_POST.equals(request.getRequestMethod())) {
-//			sendPostRequest(request.getURL(), new Listener<String>() {
-//
-//				@Override
-//				public void onResponse(String response) {
-//					Logger.i("Http response result <<<<<<<<<<<<<<< " + response);
-//					Object result = request.parseResponseResult(response);
-//					request.getOnHttpResponseListener().onHttpResponse(result);
-//				}
-//			}, new ErrorListener() {
-//
-//				@Override
-//				public void onErrorResponse(VolleyError error) {
-//					request.getOnHttpResponseListener().onError();
-//				}
-//			}, request.getParams());
-//		}
-//
-//	}
-//
-//	/**
-//	 * 发送Get请求
-//	 * @param url
-//	 * @param listener
-//	 * @param errorListener
-//	 */
-//	private void sendGetRequest(String url, Listener<String> listener, ErrorListener errorListener) {
-//		StringRequest request = new StringRequest(Request.Method.GET, url, listener, errorListener);
-//		DefaultRetryPolicy policy = new DefaultRetryPolicy(HTTP_REQUEST_TIMEOUT, HTTP_RETRY_TIMES, 1);
-//		request.setRetryPolicy(policy);
-//		mRequestQueue.add(request);
-//	}
-//
-//	/**
-//	 * 发送Post请求
-//	 * @param url
-//	 * @param listener
-//	 * @param errorListener
-//	 * @param params
-//	 */
-//	private void sendPostRequest(String url, Listener<String> listener, ErrorListener errorListener, final Map<String, String> params) {
-//		StringRequest request = new StringRequest(Request.Method.POST, url, listener, errorListener) {
-//			@Override
-//			protected Map<String, String> getParams() throws AuthFailureError {
-//				return params;
-//			}
-//		};
-//		DefaultRetryPolicy policy = new DefaultRetryPolicy(HTTP_REQUEST_TIMEOUT, HTTP_RETRY_TIMES, 1);
-//		request.setRetryPolicy(policy);
-//		mRequestQueue.add(request);
-//	}
-	
-//	/**
-//	 * 组合一个get请求串
-//	 * @param request
-//	 * @return
-//	 */
-//	private String createGetRequestUrl(HttpRequest request) {
-//		String requestUrl = request.getURL();
-//		Map<String, String> params = request.getParams();
-//		if (params.size() > 0) {
-//			StringBuilder sb = new StringBuilder();
-//			sb.append(requestUrl).append("?");
-//			for (String key : params.keySet()) {
-//				sb.append(key + "=" + params.get(key) + "&");
-//			}
-//			String str = sb.toString();
-//			requestUrl = str.substring(0, str.length() - 1);
-//		}
-//		return requestUrl;
-//	}
-	
 	public interface OnHttpResponseListener {
 		void onHttpResponse(Object result);
 		void onError();
